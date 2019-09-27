32e1a5f9817bef7e5ec6a1f89a195995e21fc12d
Guillermo Camacho <45975618+GuillermoCamacho550@users.noreply.github.com>
2019-09-27 18:02:53 +0200

    Add files via upload

---
+++ PlotConfigurations/cuts_VH2j_sequential.py
@@ -0,0 +1,206 @@
+ # cuts for VH2j analysis - G Camacho
+ # Sequential cuts for efficiency analysis
+
+_tmp = [ 
+     'Lepton_pdgId[0]*Lepton_pdgId[1] == -11*13', #electron & muon with op. charge
+       ]
+
+supercut = ' && '.join(_tmp)
+
+def addcut(name, exprs):
+    cuts[name] = ' && '.join(exprs)
+
+_tmp = [
+       '1'
+       ]
+
+addcut('VH_2j_emu_0', _tmp)
+
+_tmp = [
+       'Lepton_pt[0]>25.',
+       'Lepton_pt[1]>10.',
+       '(abs(Lepton_pdgId[1]) == 13 || Lepton_pt[1]>13.)', #if its an electron, pt2>13
+       '(nLepton>=2 && Alt$(Lepton_pt[2],0)<10.)',    
+
+       ]
+
+addcut('VH_2j_emu_1', _tmp)
+
+
+_tmp = [
+       'Lepton_pt[0]>25.',
+       'Lepton_pt[1]>10.',
+       '(abs(Lepton_pdgId[1]) == 13 || Lepton_pt[1]>13.)', #if its an electron, pt2>13
+       '(nLepton>=2 && Alt$(Lepton_pt[2],0)<10.)',    
+       'mll>12.',
+       'ptll>30',
+
+       ]
+
+addcut('VH_2j_emu_2', _tmp)
+
+_tmp = [
+       'Lepton_pt[0]>25.',
+       'Lepton_pt[1]>10.',
+       '(abs(Lepton_pdgId[1]) == 13 || Lepton_pt[1]>13.)', #if its an electron, pt2>13
+       '(nLepton>=2 && Alt$(Lepton_pt[2],0)<10.)',    
+       'mll>12.',
+       'ptll>30',
+       'PuppiMET_pt>20.',
+
+       ]
+
+addcut('VH_2j_emu_3', _tmp)
+
+_tmp = [
+       'Lepton_pt[0]>25.',
+       'Lepton_pt[1]>10.',
+       '(abs(Lepton_pdgId[1]) == 13 || Lepton_pt[1]>13.)', #if its an electron, pt2>13
+       '(nLepton>=2 && Alt$(Lepton_pt[2],0)<10.)',    
+       'mll>12.',
+       'ptll>30',
+       'PuppiMET_pt>20.',
+       'Sum$(CleanJet_pt>30.)>=2', #At least 2 jets
+       ]
+
+addcut('VH_2j_emu_4', _tmp)
+_tmp = [
+       'Lepton_pt[0]>25.',
+       'Lepton_pt[1]>10.',
+       '(abs(Lepton_pdgId[1]) == 13 || Lepton_pt[1]>13.)', #if its an electron, pt2>13
+       '(nLepton>=2 && Alt$(Lepton_pt[2],0)<10.)',    
+       'mll>12.',
+       'ptll>30',
+       'PuppiMET_pt>20.',
+       'abs(CleanJet_eta[0])<2.5',
+       'abs(CleanJet_eta[1])<2.5',
+       'Sum$(CleanJet_pt>30.)>=2', #At least 2 jets
+       
+       ]
+
+addcut('VH_2j_emu_5', _tmp)
+_tmp = [
+       'Lepton_pt[0]>25.',
+       'Lepton_pt[1]>10.',
+       '(abs(Lepton_pdgId[1]) == 13 || Lepton_pt[1]>13.)', #if its an electron, pt2>13
+       '(nLepton>=2 && Alt$(Lepton_pt[2],0)<10.)',    
+       'mll>12.',
+       'ptll>30',
+       'PuppiMET_pt>20.',
+       'abs(CleanJet_eta[0])<2.5',
+       'abs(CleanJet_eta[1])<2.5',
+       'Sum$(CleanJet_pt>30.)>=2', #At least 2 jets
+       'detajj<3.5',# delta eta 2 jets 
+       ]
+
+addcut('VH_2j_emu_6', _tmp)
+
+_tmp = [
+       'Lepton_pt[0]>25.',
+       'Lepton_pt[1]>10.',
+       '(abs(Lepton_pdgId[1]) == 13 || Lepton_pt[1]>13.)', #if its an electron, pt2>13
+       '(nLepton>=2 && Alt$(Lepton_pt[2],0)<10.)',    
+       'mll>12.',
+       'ptll>30',
+       'PuppiMET_pt>20.',
+       'abs(CleanJet_eta[0])<2.5',
+       'abs(CleanJet_eta[1])<2.5',
+       'Sum$(CleanJet_pt>30.)>=2', #At least 2 jets
+       'detajj<3.5',# delta eta 2 jets 
+       'mth>60.',
+       'mth<125.',
+
+       ]
+
+addcut('VH_2j_emu_7', _tmp)
+
+
+_tmp = [
+       'Lepton_pt[0]>25.',
+       'Lepton_pt[1]>10.',
+       '(abs(Lepton_pdgId[1]) == 13 || Lepton_pt[1]>13.)', #if its an electron, pt2>13
+       '(nLepton>=2 && Alt$(Lepton_pt[2],0)<10.)',    
+       'mll>12.',
+       'ptll>30',
+       'PuppiMET_pt>20.',
+       'abs(CleanJet_eta[0])<2.5',
+       'abs(CleanJet_eta[1])<2.5',
+       'Sum$(CleanJet_pt>30.)>=2', #At least 2 jets
+       'detajj<3.5',# delta eta 2 jets 
+       'mth>60.',
+       'mth<125.',
+       'drll<2.',   #DeltaRll<2
+
+       ]
+
+addcut('VH_2j_emu_8', _tmp)
+
+_tmp = [
+       'Lepton_pt[0]>25.',
+       'Lepton_pt[1]>10.',
+       '(abs(Lepton_pdgId[1]) == 13 || Lepton_pt[1]>13.)', #if its an electron, pt2>13
+       '(nLepton>=2 && Alt$(Lepton_pt[2],0)<10.)',    
+       'mll>12.',
+       'ptll>30',
+       'PuppiMET_pt>20.',
+       'abs(CleanJet_eta[0])<2.5',
+       'abs(CleanJet_eta[1])<2.5',
+       'Sum$(CleanJet_pt>30.)>=2', #At least 2 jets
+       'detajj<3.5',# delta eta 2 jets 
+       'mth>60.',
+       'mth<125.',
+       'drll<2.',
+       '(Sum$(CleanJet_pt > 20. && Jet_btagDeepB[CleanJet_jetIdx] > 0.1522) == 0)', #no bjets with pt>20
+
+       ]
+
+addcut('VH_2j_emu_9', _tmp)
+
+_tmp = [
+       'Lepton_pt[0]>25.',
+       'Lepton_pt[1]>10.',
+       '(abs(Lepton_pdgId[1]) == 13 || Lepton_pt[1]>13.)', #if its an electron, pt2>13
+       '(nLepton>=2 && Alt$(Lepton_pt[2],0)<10.)',    
+       'mll>12.',
+       'ptll>30',
+       'PuppiMET_pt>20.',
+       'abs(CleanJet_eta[0])<2.5',
+       'abs(CleanJet_eta[1])<2.5',
+       'Sum$(CleanJet_pt>30.)>=2', #At least 2 jets
+       'detajj<3.5',# delta eta 2 jets 
+       'mth>60.',
+       'mth<125.',
+       'drll<2.',
+       '(Sum$(CleanJet_pt > 20. && Jet_btagDeepB[CleanJet_jetIdx] > 0.1522) == 0)', #no bjets with pt>20
+       'mjj>65.',
+       'mjj<105.',
+
+       ]
+
+addcut('VH_2j_emu_10', _tmp)
+
+_tmp = [
+       'Lepton_pt[0]>25.',
+       'Lepton_pt[1]>10.',
+       '(abs(Lepton_pdgId[1]) == 13 || Lepton_pt[1]>13.)', #if its an electron, pt2>13
+       '(nLepton>=2 && Alt$(Lepton_pt[2],0)<10.)',    
+       'mll>12.',
+       'ptll>30',
+       'PuppiMET_pt>20.',
+       'abs(CleanJet_eta[0])<2.5',
+       'abs(CleanJet_eta[1])<2.5',
+       'Sum$(CleanJet_pt>30.)>=2', #At least 2 jets
+       'detajj<3.5',# delta eta 2 jets 
+       'mth>60.',
+       'mth<125.',
+       'drll<2.',
+       '(Sum$(CleanJet_pt > 20. && Jet_btagDeepB[CleanJet_jetIdx] > 0.1522) == 0)', #no bjets with pt>20
+       'mjj>65.',
+       'mjj<105.',
+       'Jet_qgl[CleanJet_jetIdx[0]]>0.4', #QGL cuts 31/5/19
+       'Jet_qgl[CleanJet_jetIdx[1]]>0.3',
+
+       ]
+
+addcut('VH_2j_emu_11', _tmp)
+
